e7d4bb16f25475798b1e19b6a0bef95bedb3d566
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2025-10-28 21:57:23 +0300

    Add SubmissionDto classes for exam submission

---
+++ SmartCampus.App/DTOs/SubmissionDto.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCampus.App.Dtos
+
+{
+    public class ExamAnswerDto
+    {
+
+        public int QuestionId { get; set; }
+        public bool? TrueFalseAnswer { get; set; }
+        public int? SelectedOptionId { get; set; }
+    }
+
+
+    public class SubmitExamDto
+    {
+        public int ExamId { get; set; }
+        public int StudentId { get; set; }
+        public List<ExamAnswerDto> Answers { get; set; }= new List<ExamAnswerDto>();
+
+
+    }
+
+
+    public class ExamResultDto
+    {
+        public int ExamId { get; set; }
+        public int StudentId { get; set; }
+
+        public decimal Score { get; set; }
+
+        public int CorrectAnswers { get; set; }
+        public int TotalQuestions { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+    }
+
+
+}
